ba831680fc17a0bed08c98ba344e26f7225479db
gregtr <t.grzegorz90@gmail.com>
2017-08-15 14:16:08 +0100

    XML Reader Tut

---
+++ CSharpQuickReference/CSharpQuickReference/src/XMLReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CSharpQuickReference.src
+{
+    class XMLReader
+    {
+        //http://csharp.net-tutorials.com/xml/reading-xml-with-the-xmlreader-class/
+        public static void loadFileDummy()
+        {
+            XmlReader xmlReader = XmlReader.Create("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
+            while (xmlReader.Read())
+            {
+                if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Cube"))
+                {
+                    if (xmlReader.HasAttributes)
+                        Console.WriteLine(xmlReader.GetAttribute("currency") + ": " + xmlReader.GetAttribute("rate"));
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
