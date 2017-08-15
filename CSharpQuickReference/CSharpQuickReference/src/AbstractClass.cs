6d2ca132edb064f2ac34299f91fa5b74f567ef92
Damjan <Damjan@Damjan-Alpine.Alpineinteractive.co.uk>
2017-08-15 10:22:06 +0100

    Additional basics

---
+++ CSharpQuickReference/CSharpQuickReference/src/AbstractClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpQuickReference.src
+{
+    // great for frameworks - naming should describe concept - abstract concept
+    abstract class AbstractClass
+    {
+        public virtual string Describe()
+        {
+            return "Just default text";
+        }
+
+        public abstract string abstractMethod();
+    } 
+    // to override inherited method , base method needs to be marked as vitual, abstract, or override
+    // to disallov futher overrides - we can use sealed override keyword
+    
+    // no instance of abstract class can be created
+    class ObjClass : AbstractClass
+    {
+        public override string Describe()
+        {
+            string result = base.Describe();
+            result += " - same but different";
+            return result;
+        }
+
+        public override string abstractMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharpQuickReference/CSharpQuickReference/src/Exceptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpQuickReference.src
+{
+    class Exceptions
+    {
+        public static void testException(){
+            int[] numbers = new int[2];
+            try
+            {
+                numbers[0] = 12;
+                numbers[1] = 124;
+                numbers[2] = 1234; // out of the bounds exception ERROR
+
+                foreach (int i in numbers)
+                {
+                    Console.WriteLine(i);
+                }
+            }// parameters for catch are optional
+            catch (IndexOutOfRangeException ex) { Console.WriteLine("Out of the range exception"); }
+            catch(Exception ex) { Console.WriteLine("Exception occured " + ex.Message + " - " + ex.GetType().ToString()); }
+            finally { Console.WriteLine("This will always show up"); }
+        }
+    }
+}
