a8d3c38846e10abec4df308aedf2cd72e967e23d
gregtr <t.grzegorz90@gmail.com>
2017-08-24 17:10:13 +0100

    some more boring stuff

---
+++ Vanilla/HTML CSS/scripts/jsScript.js
@@ -0,0 +1,20 @@
+/*
+ In HTML , JavaScript code must be inserted between <script> tags.
+ By default in html javascript is scripting language.
+ 
+ HINT:
+ Placing scripts at the bottom of the <body> element improves the display speed, because script compilation slows down the display.
+	External JavaScript Advantages
+		It separates HTML and code
+		It makes HTML and JavaScript easier to read and maintain
+		Cached JavaScript files can speed up page loads
+*/
+
+/*		
+		Javascript Display possibilities
+
+	innerHTM 			-> writing into HTML element
+	document.write()    -> writing into HTML output
+	window.alert() 		-> writing into an alert box
+	console.log()		-> writing into broswer console
+*/
