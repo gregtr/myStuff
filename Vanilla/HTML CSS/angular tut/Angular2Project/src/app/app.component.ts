1e195f9a45ecaeadc94013489fefad5bd64e1e1d
gregtr <t.grzegorz90@gmail.com>
2017-09-05 15:59:13 +0100

    Created new component 'postsComponent'

--- Vanilla/HTML CSS/angular tut/Angular2Project/src/app/app.component.ts
+++ Vanilla/HTML CSS/angular tut/Angular2Project/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // we can change this prefix to package file ( and we know why there is a hyphen )
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '***Super duper tutorial for angular - just for fun and for future.***';
 }
